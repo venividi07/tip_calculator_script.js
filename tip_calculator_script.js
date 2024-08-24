9744c305628077ceb4935ea95fd3aaf059d515f7
venividi07 <157637070+venividi07@users.noreply.github.com>
2024-08-24 19:40:40 +0300

    Add files via upload

---
+++ tip_calculator_script.js
@@ -0,0 +1,11 @@
+let bill = 275;
+let tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log (`Factura este ${bill}, bacsisul: ${tip}, total spre plata ${bill + tip}`);
+
+bill = 40;
+tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log (`Factura este ${bill}, bacsisul: ${tip}, total spre plata ${bill + tip}`);
+
+bill = 430;
+tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log (`Factura este ${bill}, bacsisul: ${tip}, total spre plata ${bill + tip}`);
